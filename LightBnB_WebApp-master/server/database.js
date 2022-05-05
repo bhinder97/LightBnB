46ad10bb49e6a18641e90da8e28fbabe6f667466
Harsimran Bhinder <bhinderrr@gmail.com>
2022-05-05 19:57:24 +0000

    adding properties to lighthouseBNB

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -179,9 +179,22 @@ exports.getAllProperties = getAllProperties;
  * @return {Promise<{}>} A promise to the property.
  */
 const addProperty = function(property) {
-  const propertyId = Object.keys(properties).length + 1;
-  property.id = propertyId;
-  properties[propertyId] = property;
-  return Promise.resolve(property);
-}
+  const queryParams = [...Object.values(property)];
+  const queryString = `
+  INSERT INTO properties(title, description, number_of_bedrooms, number_of_bathrooms, parking_spaces, cost_per_night, thumbnail_photo_url,
+    cover_photo_url, street, country, city, province, post_code, owner_id)
+    VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10, $11, $12, $13, $14)
+    RETURNING *;
+    `;
+  console.log(queryParams);
+  return new Promise((resolve, reject) => {
+    pool.query(queryString, queryParams)
+    .then(data => {
+      resolve(data.rows);
+    })
+    .catch(error => {
+      reject(error);
+    });
+  });
+};
 exports.addProperty = addProperty;
